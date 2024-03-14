5ec5a59abd16f3afcefa7e0721cde63910bf89ad
xkolumber <lubosk158@gmail.com>
2024-03-14 16:34:42 +0100

    shop section show all product

--- src/app/components/ShopSection.tsx
+++ src/app/components/ShopSection.tsx
@@ -4,12 +4,19 @@ import Link from "next/link";
 import Image from "next/image";
 import { urlFor } from "../lib/sanityImageUrl";
 import { ClipLoader } from "react-spinners";
-import { Product } from "../lib/all_interfaces";
+import { Product, ProductFirebase } from "../lib/all_interfaces";
+import useCartStore from "../counter/store";
+import toast, { Toaster } from "react-hot-toast";
+import IconMinus from "./IconMinus";
+import IconPlus from "./IconPlus";
 
 const ShopSection = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("");
-  const [filteredData, setFilteredData] = useState<Product[]>([]);
+  const [filteredData, setFilteredData] = useState<ProductFirebase[]>([]);
+  const addToCart = useCartStore((state) => state.addToCart);
   const [isLoading, setIsLoading] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [quantity, setQuantity] = useState([1, 1, 1]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,6 +36,15 @@ const ShopSection = () => {
 
     fetchData();
   }, []);
+  useEffect(() => {
+    if (filteredData) {
+      setQuantity(new Array(filteredData.length).fill(1));
+    }
+  }, [filteredData]);
+
+  const handleOpacity = (index: number) => {
+    setHoveredIndex(index);
+  };
 
   const handleCategoryChange = async (
     event: React.ChangeEvent<HTMLSelectElement>
@@ -61,6 +77,30 @@ const ShopSection = () => {
       setIsLoading(false);
     }
   };
+
+  const increaseQuantity = (index: number) => {
+    setQuantity((prevQuantity) => {
+      const newQuantity = [...prevQuantity];
+      newQuantity[index] = newQuantity[index] + 1;
+      return newQuantity;
+    });
+  };
+
+  const decreaseQuantity = (index: number) => {
+    if (quantity[index] > 1) {
+      setQuantity((prevQuantity) => {
+        const newQuantity = [...prevQuantity];
+        newQuantity[index] = newQuantity[index] - 1;
+        return newQuantity;
+      });
+    }
+  };
+
+  const handleAddToCart = (id: string, quantity: number) => {
+    addToCart({ id, quantity });
+    toast.success("Pridané do košíka");
+  };
+
   const categories = [
     "",
     "Antioxidanty",
@@ -90,6 +130,7 @@ const ShopSection = () => {
 
   return (
     <div className="main_section mt-32 md:mt-0">
+      <Toaster />
       <h2 className="uppercase">Obchod</h2>
 
       <select name="categories" id="categories" onChange={handleCategoryChange}>
@@ -99,24 +140,83 @@ const ShopSection = () => {
           </option>
         ))}
       </select>
-      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8  mt-8 md:mt-24">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 md:mt-24">
         {isLoading ? (
-          <ClipLoader size={40} color={"#174218"} loading={isLoading} />
+          <div className="min-h-[500px]">
+            <ClipLoader size={40} color={"#174218"} loading={isLoading} />
+          </div>
         ) : (
+          filteredData &&
           filteredData.map((item, index) => (
-            <Link href={`/obchod/produkt/${item.slug.current}`} key={index}>
-              <div className="flex flex-col items-center">
-                <Image
-                  src={urlFor(item.photo).url()}
-                  width={500}
-                  height={500}
-                  priority={true}
-                  className="w-full h-full rounded-xl"
-                  alt="Produktový obrázok"
-                />
-                <p className="text-secondary pt-4 xl:pt-8">{item.title}</p>
+            <div key={index}>
+              <Link
+                href={`/obchod/produkt/${item.slug}`}
+                className="flex flex-col h-[400px]"
+              >
+                <div
+                  className="flex flex-col items-center bg-fifthtiary rounded-xl w-full h-full justify-center relative"
+                  onMouseEnter={() => handleOpacity(index)}
+                  onMouseLeave={() => handleOpacity(-1)}
+                >
+                  <Image
+                    src={item.produkt_pozadie}
+                    width={0}
+                    height={0}
+                    priority={true}
+                    quality={100}
+                    sizes="100vw"
+                    className={`absolute w-full h-full object-cover transition-opacity ${
+                      hoveredIndex === index ? "opacity-100" : "opacity-40"
+                    } z-10 ease-in `}
+                    alt="Produktový obrázok"
+                  />
+                  <Image
+                    src={item.produkt_foto}
+                    width={500}
+                    height={500}
+                    priority={true}
+                    quality={100}
+                    className="w-full h-[200px]  object-contain z-[1000] "
+                    alt="Produktový obrázok"
+                  />
+                </div>{" "}
+              </Link>
+              <div className="flex flex-col w-full justify-center items-center">
+                <div className="flex flex-col w-[100%]">
+                  <p className="text-black pt-4 uppercase font-semibold">
+                    {item.nazov}
+                  </p>
+                  <p>{item.cena} €</p>
+                  <div className="flex flex-row justify-between items-center">
+                    <p className="uppercase font-medium">Počet kusov</p>
+                    <div className="flex flex-row items-center gap-4  ml-12 md:ml-0 scale-125 md:scale-100">
+                      <div
+                        className="cursor-pointer"
+                        onClick={() => decreaseQuantity(index)}
+                      >
+                        <IconMinus />
+                      </div>
+
+                      <div className="border border-secondary pt-2 pb-2 pl-8 pr-8 rounded-[32px] text-secondary">
+                        {quantity[index]}
+                      </div>
+                      <div
+                        className="cursor-pointer"
+                        onClick={() => increaseQuantity(index)}
+                      >
+                        <IconPlus />
+                      </div>
+                    </div>
+                    <button
+                      className="btn btn--fourthtiary"
+                      onClick={() => handleAddToCart(item.id, quantity[index])}
+                    >
+                      Kúpiť
+                    </button>
+                  </div>
+                </div>
               </div>
-            </Link>
+            </div>
           ))
         )}
       </div>
