dcd916c2a2aad6aa0d9bc46d36f34b895034ccc8
xkolumber <lubosk158@gmail.com>
2024-09-23 17:24:10 +0200

    newer next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["cdn.sanity.io", "firebasestorage.googleapis.com"],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
